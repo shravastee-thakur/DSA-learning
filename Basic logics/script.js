1481b6429a474f9cd61ec63d131cc5e4753f5b1e
Shravastee Thakur <shravastee@gmail.com>
2026-05-15 13:11:05 +0530

    Basic logics in JS

---
+++ Basic logics/script.js
@@ -0,0 +1,41 @@
+// let age = prompt("What is your age");
+// console.log(age);
+
+// here output of prompt function is always string therefore to make it number
+
+// let age = Number(prompt("What is your age")); // known as type castng
+// console.log(age);
+
+// Swapping
+// method 1:
+// let a = 10;
+// let b = 20;
+// let c;
+
+// c = a;
+// a = b;
+// b = c;
+
+// console.log("Now a is", a);
+// console.log("Now b is", b);
+
+// method 2:
+// let a = 10;
+// let b = 20;
+
+// a = a + b; // a = 30
+// b = a - b; // 30 - 20 = 10
+// a = a - b; // 30 - 10 = 20
+
+// console.log("Now a is", a);
+// console.log("Now b is", b);
+
+//method 3: (destructuring assignment in JavaScript)
+// let a = 10;
+// let b = 20;
+
+// [a, b] = [b, a]; //Swappig values
+// console.log("Now a is", a);
+// console.log("Now b is", b);
+
+// -----x-------
